535382084a6371bb2efa14380a27be7451f81707
Lindsay Sarson <lindsaysarson@trentu.ca>
2022-10-18 15:35:07 -0400

    test commit

---
+++ Desktop/omgapp/android/app/src/main/kotlin/com/example/omgapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.omgapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Desktop/omgapplication/android/app/src/main/kotlin/com/example/omgapplication/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.omgapplication
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
